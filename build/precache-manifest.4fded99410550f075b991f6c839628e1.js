ef39f0b00b065ab074f0915c86923ac1d077e7e3
degraffa <ard96@cornell.edu>
2020-05-19 10:53:43 -0400

    New build

---
+++ build/precache-manifest.4fded99410550f075b991f6c839628e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ece7e5ce0d313320b541569bcb7387a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8165abe79f3dd99ea7fa",
+    "url": "/static/css/main.22ffd47c.chunk.css"
+  },
+  {
+    "revision": "cb6b341c8b2d5998f5e7",
+    "url": "/static/js/2.8016843d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8016843d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8165abe79f3dd99ea7fa",
+    "url": "/static/js/main.41af5b37.chunk.js"
+  },
+  {
+    "revision": "972c30427e80b88643c6",
+    "url": "/static/js/runtime-main.21fc0980.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92c56930c487380d41b11872aabd6e2e.js"
+  "/precache-manifest.4fded99410550f075b991f6c839628e1.js"
 );
 
 self.addEventListener('message', (event) => {
