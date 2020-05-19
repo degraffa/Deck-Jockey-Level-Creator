563771b531679a87d504ab05fd3d346017d59c62
degraffa <ard96@cornell.edu>
2020-05-19 07:47:27 -0400

    Beginning of something beautiful

---
+++ build/precache-manifest.92c56930c487380d41b11872aabd6e2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918da76066d2094f6b94aff135c11ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b59e4e76813ffa4c228",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9fa237e36ec8e426482",
+    "url": "/static/js/2.e39dd0c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e39dd0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b59e4e76813ffa4c228",
+    "url": "/static/js/main.61673b23.chunk.js"
+  },
+  {
+    "revision": "972c30427e80b88643c6",
+    "url": "/static/js/runtime-main.21fc0980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+class Card {
+  constructor(genre, pointsPerLoop, difficulty, loopName) {
+    this.genre = genre;
+    this.pointsPerLoop = pointsPerLoop;
+    this.difficulty = difficulty;
+    this.loopName = loopName;
+  }
+}
+
+class Recipe {
+  constructor(genre0, barPoints0, genre1, barPoints1, genre2, barPoints2) {
+    this.genre0 = genre0;
+    this.barPoints0 = barPoints0;
+    this.genre1 = genre1;
+    this.barPoints1 = barPoints1;
+    this.genre2 = genre2;
+    this.barPoints2 = barPoints2;
+  }
+}
+
+class CardForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      genre: "ROCK",
+      pointsPerLoop: 0.0,
+      difficulty: 1,
+      loopName: "funky"
+    }
+  }
+}
+
+export class LevelForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      levelName: "",
+      levelTimer: 0,
+      cards: [],
+      recipes: []
+    };
+  }
+
+  changeName = (event) => {
+    this.setState({levelName: event.target.value});
+  }
+
+  changeTimer(event) {
+    this.setState(this.state.levelTimer, event.target.value);
+  }
+
+  addCard() {
+    let card = Card(0, 0, 0,0);
+    this.state.cards.push(card);
+  }
+
+  changeCard(cardIdx) {
+    let card = Card(0, 0, 0, 0);
+    this.setState(this.state.cards[cardIdx], card);
+  }
+
+  addRecipe() {
+    let recipe = Recipe(0, 0, 0, 0, 0, 0);
+    this.state.recipes.push(recipe);
+  }
+
+  changeRecipe(recipeIdx) {
+    let recipe = Recipe(0, 0, 0, 0, 0, 0);
+    this.setState(this.state.recipes[recipeIdx], recipe);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("" + this.state.levelName);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Level Name: </label> 
+        <input type="text" value={this.state.levelName} onChange={this.changeName}/> <br />
+        <input type="submit" value="Get Level JSON" />
+      </form>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        <LevelForm />
       </header>
     </div>
   );
 }
+
+
 
 export default App;
