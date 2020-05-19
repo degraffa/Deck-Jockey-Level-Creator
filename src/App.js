eb1c34cc16ae230a70041cab5a5e6fd0a245644a
degraffa <ard96@cornell.edu>
2020-05-19 08:55:18 -0400

    Add new cards

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-class Card {
-  constructor(genre, pointsPerLoop, difficulty, loopName) {
-    this.genre = genre;
-    this.pointsPerLoop = pointsPerLoop;
-    this.difficulty = difficulty;
-    this.loopName = loopName;
-  }
-}
-
-class Recipe {
-  constructor(genre0, barPoints0, genre1, barPoints1, genre2, barPoints2) {
-    this.genre0 = genre0;
-    this.barPoints0 = barPoints0;
-    this.genre1 = genre1;
-    this.barPoints1 = barPoints1;
-    this.genre2 = genre2;
-    this.barPoints2 = barPoints2;
-  }
-}
-
 class CardForm extends React.Component {
   constructor(props) {
     super(props);
@@ -32,15 +12,38 @@ class CardForm extends React.Component {
       loopName: "funky"
     }
   }
+
+  changeGenre = (event) => {
+    this.setState({genre: this.state.value});
+  }
+
+  render() {
+    return (
+      <form>
+        <label>Card #{this.props.cardIdx}: </label>
+        <label>Genre: </label>
+        <select value={this.state.genre} onChange={this.changeGenre}>
+          <option value="ROCK">ROCK</option>
+          <option value="JAZZ">JAZZ</option>
+          <option value="HIPHOP">HIPHOP</option>
+          <option value="FOLK">FOLK</option>
+        </select>
+        <label> Points Per Loop: </label>
+        <input type="text" value={this.state.pointsPerLoop} style={{width: 30, textAlign: "center"}} />
+        <label> Loop Multiplier: </label>
+        <input type="text" value={this.state.pointsPerLoop} style={{width: 30, textAlign: "center"}} />
+      </form>
+    )
+  }
 }
 
 export class LevelForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      levelName: "",
+      levelName: "Example Level",
       levelTimer: 0,
-      cards: [],
+      cards: [<CardForm cardIdx="0" />],
       recipes: []
     };
   }
@@ -52,37 +55,47 @@ export class LevelForm extends React.Component {
   changeTimer(event) {
     this.setState(this.state.levelTimer, event.target.value);
   }
-
-  addCard() {
-    let card = Card(0, 0, 0,0);
-    this.state.cards.push(card);
-  }
-
-  changeCard(cardIdx) {
-    let card = Card(0, 0, 0, 0);
-    this.setState(this.state.cards[cardIdx], card);
-  }
-
-  addRecipe() {
-    let recipe = Recipe(0, 0, 0, 0, 0, 0);
-    this.state.recipes.push(recipe);
-  }
-
-  changeRecipe(recipeIdx) {
-    let recipe = Recipe(0, 0, 0, 0, 0, 0);
-    this.setState(this.state.recipes[recipeIdx], recipe);
-  }
-
   handleSubmit = (event) => {
     event.preventDefault();
     alert("" + this.state.levelName);
+  }
+
+  addCard = () => {
+    let numCards = this.state.cards.length;
+    this.state.cards.push(<CardForm cardIdx={numCards+1} />);
+    this.setState({cards: this.state.cards});
+    alert(numCards);
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>Level Name: </label> 
-        <input type="text" value={this.state.levelName} onChange={this.changeName}/> <br />
+        <input type="text" value={this.state.levelName} onChange={this.changeName}/>
+        
+        <br />
+        <label>Level Timer: </label>
+        
+        <input type="text" value={this.state.levelTimer} onChange={this.changeTimer}/>
+        <br />
+
+        <br />
+        <label> Cards </label> <button type="button" onClick={this.addCard}>New Card</button>
+        <br />
+        <div id="cards">
+          {this.state.cards.map(card => {
+            return(
+              <CardForm cardIdx="0" />
+            )
+          })}
+        </div> 
+
+        <br/>
+        <label>Recipes</label>
+        <br />
+        <div id="recipes">
+
+        </div> 
         <input type="submit" value="Get Level JSON" />
       </form>
     )
